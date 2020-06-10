892ccf6ce71c17c798a4b51e5771eefde340ad4e
sck994415418 <994415418@qq.com>
2020-06-10 14:47:02 +0800

    sck

--- app/admin/controller/Client.php
+++ app/admin/controller/Client.php
@@ -62,10 +62,12 @@ class Client extends Permissions
         $result = array_intersect($position,$street_ids);
         $data = empty($where) ? $model
             ->where(['client_position_id' => ['in', $result]])
+            ->where(['client_type' => 1])
             ->order('create_time desc')
             ->paginate(20)
             : $model->where($where)
                 ->where(['client_position_id' => ['in', $result]])
+                ->where(['client_type' => 1])
                 ->order('create_time desc')
                 ->paginate(20, false, ['query' => $this->request->param()]);
 
@@ -114,11 +116,12 @@ class Client extends Permissions
                     $this->error('提交失败：' . $validate->getError());
                 }
                 if (!empty($post['client_phone'])) {
-                    $name = $model->where(['client_phone' => $post['client_phone'], 'client_id' => ['neq', $post['client_id']]])->select();
+                    $name = $model->where(['client_phone' => $post['client_phone'], 'client_id' => ['neq', $post['client_id'],'client_type'=>1]])->select();
                 }
                 if (!empty($name)) {
                     return $this->error('提交失败：该客户手机已被添加');
                 }
+                $post['client_type'] = 1;
                 if (false == $model->allowField(true)->save($post, ['client_id' => $client_id])) {
                     return $this->error('修改失败');
                 } else {
@@ -140,11 +143,12 @@ class Client extends Permissions
                 if (!$validate->check($post)) {
                     $this->error('提交失败：' . $validate->getError());
                 }
-                $name = $model->where(['client_phone' => $post['client_phone']])->select();
+                $name = $model->where(['client_phone' => $post['client_phone'],'client_type'=>1])->select();
                 if (!empty($name)) {
                     return $this->error('提交失败：该客户手机已被添加');
                 }
                 $post['create_time'] = time();
+                $post['client_type'] = 1;
                 if (false == $model->allowField(true)->save($post)) {
                     return $this->error('添加失败');
                 } else {
@@ -190,6 +194,155 @@ class Client extends Permissions
             }
         } else {
             return $this->error('页面错误，请重试！');
+        }
+    }
+
+//渠道客户
+    public function channl()
+    {
+        $model = new ClientModel();
+        $post = $this->request->param();
+        if (isset($post['keywords']) and !empty($post['keywords'])) {
+            $where['client_name|client_phone|client_wechat'] = ['like', '%' . $post['keywords'] . '%'];
+        }
+        if (isset($post['client_position_id']) and $post['client_position_id'] > 0) {
+            $new_client_position_id = substr($post['client_position_id'], 0, $post['num']);
+            $where['client_position_id'] = ['like', $new_client_position_id . '%'];
+        }
+        if (isset($post['create_time']) and !empty($post['create_time'])) {
+            $min_time = strtotime($post['create_time']);
+            $max_time = $min_time + 24 * 60 * 60;
+            $where['create_time'] = [['>=', $min_time], ['<=', $max_time]];
+        }
+        if (isset($post['time']) and !empty($post['time'])) {
+            $start_time = strtotime(substr($post['time'], 0, strripos($post['time'], ' - ')));
+            $end_time = strtotime(substr($post['time'], strripos($post['time'], ' - ') + 3));
+            $where['create_time'] = ['between', [$start_time, $end_time]];
+        }
+        $id = Session::get('admin');
+        if (!empty($id)) {
+            $user_info = \app\admin\model\Admin::get($id);
+            if (!empty($user_info->address_ids)) {
+                $address_ids = json_decode($user_info->address_ids, true);
+                if (!empty($address_ids)) {
+                    $address_ids = db('address')->where(['status' => 1, 'id4' => ['in', $address_ids]])->column('id4');
+                } else {
+                    $address_ids = [];
+                }
+            } else {
+                $address_ids = [];
+            }
+        } else {
+            $address_ids = [];
+        }
+        if (!empty($address_ids)) {
+            $street_ids = db('address')
+                ->where(['id1' => 1, 'status' => 1, 'id4' => ['in', $address_ids]])
+                ->column('id5');
+            $street_ids = array_filter($street_ids);
+
+        } else {
+            $street_ids = [];
+        }
+        $position = $model->column('client_position_id');
+        $result = array_intersect($position,$street_ids);
+        $data = empty($where) ? $model
+            ->where(['client_position_id' => ['in', $result]])
+            ->where(['client_type' => 2])
+            ->order('create_time desc')
+            ->paginate(20)
+            : $model->where($where)
+                ->where(['client_position_id' => ['in', $result]])
+                ->where(['client_type' => 2])
+                ->order('create_time desc')
+                ->paginate(20, false, ['query' => $this->request->param()]);
+
+        $address = db('address')
+            ->field('id1,id2 as address_id, name2 as address_name')
+            ->where(['id1' => 1, 'status' => 1, 'id4' => ['in', $address_ids]])
+            ->group('address_id')
+            ->select();
+        $this->assign('View_address', $address);
+        $this->assign('data', $data);
+        return $this->fetch();
+    }
+    //渠道客户添加/修改
+    public function ChannlPublish()
+    {
+        $client_id = $this->request->has('client_id') ? $this->request->param('client_id', 0, 'intval') : 0;
+        $model = new ClientModel();
+        $id = Session::get('admin');
+        if (!empty($id)) {
+            $user_info = \app\admin\model\Admin::get($id);
+            if (!empty($user_info->address_ids)) {
+                $address_ids = json_decode($user_info->address_ids, true);
+            } else {
+                $address_ids = [];
+            }
+        } else {
+            $address_ids = [];
+        }
+
+        $address = db('address')
+            ->field('id1,id2 as address_id, name2 as address_name')
+            ->where(['id1' => 1, 'status' => 1, 'id4' => ['in', $address_ids]])
+            ->group('address_id')
+            ->select();
+        $this->assign('View_address', $address);
+
+        if ($client_id > 0) {
+            if ($this->request->isPost()) {
+                $post = $this->request->post();
+                $validate = new Validate([
+//                    ['client_name', 'require', '客户姓名不能为空'],
+//                    ['client_phone', 'require', '客户电话不能为空'],
+                ]);
+                if (!$validate->check($post)) {
+                    $this->error('提交失败：' . $validate->getError());
+                }
+                if (!empty($post['client_phone'])) {
+                    $name = $model->where(['client_phone' => $post['client_phone'], 'client_id' => ['neq', $post['client_id']],'client_type'=>2])->select();
+                }
+                if (!empty($name)) {
+                    return $this->error('提交失败：该客户手机已被添加');
+                }
+                $post['client_type'] = 2;
+                if (false == $model->allowField(true)->save($post, ['client_id' => $client_id])) {
+                    return $this->error('修改失败');
+                } else {
+                    addlog($model->client_id);
+                    return $this->success('修改信息成功', 'admin/client/channl');
+                }
+            } else {
+                $data = $model->where('client_id', $client_id)->find();
+                $this->assign('data', $data);
+                return $this->fetch();
+            }
+        } else {
+            if ($this->request->isPost()) {
+                $post = $this->request->post();
+                $validate = new Validate([
+                    ['client_name', 'require', '客户姓名不能为空'],
+                    ['client_phone', 'require', '客户电话不能为空'],
+                ]);
+                if (!$validate->check($post)) {
+                    $this->error('提交失败：' . $validate->getError());
+                }
+                $name = $model->where(['client_phone' => $post['client_phone'],'client_type'=>2])->select();
+                if (!empty($name)) {
+                    return $this->error('提交失败：该客户手机已被添加');
+                }
+                $post['create_time'] = time();
+                $post['client_type'] = 2;
+                if (false == $model->allowField(true)->save($post)) {
+                    return $this->error('添加失败');
+                } else {
+                    addlog($model->client_id);
+                    return $this->success('添加成功', 'admin/client/channl');
+                }
+            } else {
+                return $this->fetch();
+            }
         }
     }
 }
